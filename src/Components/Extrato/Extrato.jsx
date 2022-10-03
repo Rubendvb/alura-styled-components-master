c8ad53daf351e2a10b99e95d4323559f45182359
Rubendvb <rubenvasquez556@gmail.com>
2022-10-03 00:45:36 -0300

    Criando componentes item e itens

--- src/Components/Extrato/Extrato.jsx
+++ src/Components/Extrato/Extrato.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { Botao, Box } from "../UI";
 import { extratoLista } from "../../info";
+import Itens from "../Itens/Itens";
 
 export function Extrato() {
   return (
     <Box>
       {extratoLista.updates.map(({ id, type, from, value, date }) => {
         return (
-          <div key={id}>
-            <div>{type}</div>
-            <div>{from}</div>
-          </div>
+          <Itens key={id} type={type} from={from} value={value} date={date} />
         );
       })}
+
       <Botao>Ver mais</Botao>
     </Box>
   );
